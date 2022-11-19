5375859afbef327c78071734918a1d760126b4ac
Chinmaya Patel <chinmayarulzz@gmail.com>
2022-11-19 12:39:04 +0530

    Updated configs for db connection

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
+	'hostname' => 'localhost:1234',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'rudchin_educo',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
