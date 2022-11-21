b3216cbb6b3126e80a64156750edfe899e471349
Chinmaya Patel <chinmayarulzz@gmail.com>
2022-11-21 10:23:27 +0530

    Updated links for all pages

--- application/views/admin_dashboard.php
+++ application/views/admin_dashboard.php
@@ -284,36 +284,36 @@ body {
 <header class="">
   <nav class="navbar navbar-expand-lg">
 	<div class="container">
-	  <a class="navbar-brand" href="homepage.php"><h2>RudChin<em> Educo</em></h2></a>
+	  <a class="navbar-brand" href="<?=base_url()?>"><h2>RudChin<em> Educo</em></h2></a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	  </button>
 	  <div class="collapse navbar-collapse" id="navbarResponsive">
 		<ul class="navbar-nav ml-auto">
 		<li class="nav-item active">
-			<a class="nav-link" href="dashboard.php">Home
+			<a class="nav-link" href="<?=base_url()?>admin_dashboard.php">Home
 			  <span class="sr-only">(current)</span>
 			</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link" href="blog.php">Blog</a>
+			<a class="nav-link" href="<?=base_url()?>blog">Blog</a>
 		  </li>
 
 		  <li class="nav-item">
-			<a class="nav-link" href="about.php">About Us</a>
+			<a class="nav-link" href="<?=base_url()?>about">About Us</a>
 		  </li>
 
 		  <li class="nav-item">
-			<a class="nav-link" href="team.php">Authors</a>
+			<a class="nav-link" href="<?=base_url()?>team">Authors</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link" href="contact.php">Contact Us</a>
+			<a class="nav-link" href="<?=base_url()?>contact">Contact Us</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link" href="edit_profile.php">USER</a>
+			<a class="nav-link" href="<?=base_url()?>edit_profile">USER</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link" href="homepage.php">Log Out</a>
+			<a class="nav-link" href="<?=base_url()?>homepage">Log Out</a>
 		  </li>
 		</ul>
 	  </div>
